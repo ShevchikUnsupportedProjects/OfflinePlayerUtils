73d3c9479f5a07285951ab5c5f9bdbe6546a7f06
Shevchik <Shev4ik.den@gmail.com>
2014-04-09 19:26:01 +0400

    Add interface to operate with exp data

---
+++ src/offlineplayerutils/internal/ExpDataInterface.java
@@ -0,0 +1,38 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package offlineplayerutils.internal;
+
+import org.bukkit.OfflinePlayer;
+
+public interface ExpDataInterface {
+
+	public float getExp(OfflinePlayer player);
+
+	public void setExp(OfflinePlayer player, float exp);
+
+	public int getLevel(OfflinePlayer player);
+
+	public void setLevel(OfflinePlayer player, int level);
+
+	public int getExpToLevel(OfflinePlayer player);
+
+	public void giveExp(OfflinePlayer player, int exp);
+	
+	public void giveExpLevels(OfflinePlayer player, int levels);
+
+}
---
+++ src/offlineplayerutils/internal/v1_7_R2/ExpData.java
@@ -0,0 +1,52 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+package offlineplayerutils.internal.v1_7_R2;
+
+import org.bukkit.OfflinePlayer;
+
+public class ExpData {
+
+	public float getExp(OfflinePlayer player) {
+		return 0;
+	}
+
+	public void setExp(OfflinePlayer player, float exp) {
+
+	}
+
+	public int getLevel(OfflinePlayer player) {
+		return 0;
+	}
+
+	public void setLevel(OfflinePlayer player, int level) {
+
+	}
+
+	public int getExpToLevel(OfflinePlayer player) {
+		return 0;
+	}
+
+	public void giveExp(OfflinePlayer player, int exp) {
+
+	}
+	
+	public void giveExpLevels(OfflinePlayer player, int levels) {
+
+	}
+
+}
